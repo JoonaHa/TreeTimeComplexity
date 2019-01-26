6b3821cb088651a0f70563ac044f85d4632c988b
JoonaHa <joona.halonen@helsinki.fi>
2019-01-26 23:52:24 +0200

    Added more comments

--- TreeTimeComplexity/src/main/java/algorithms/MinBinaryHeap.java
+++ TreeTimeComplexity/src/main/java/algorithms/MinBinaryHeap.java
@@ -3,7 +3,7 @@ package algorithms;
 import java.util.ArrayList;
 
 /**
- *
+ *Min Binary Heap which extends abstract class Heaps
  * @author JoonaHa
  */
 public class MinBinaryHeap extends Heaps {
@@ -12,13 +12,20 @@ public class MinBinaryHeap extends Heaps {
         super(list);
     }
 
+    /**Inserts a value to the heap
+     * 
+     * @param insert 
+     */
     @Override
     public void insert(int insert) {
 
+        // insert node to the end of the list and get the last index
         this.heap.add(insert);
         int last = this.heap.size() - 1;
         int index = last;
 
+        //if the inserted node isn't the first note 
+        //siftup to keep the heap on order
         if (last != 0) {
 
             siftUp(index);
@@ -26,12 +33,18 @@ public class MinBinaryHeap extends Heaps {
 
     }
 
+    /**
+     * Delete a node from given index and keep the heap in order
+     * @param index of a node to be removed
+     */
     @Override
     public void delete(int index) {
 
+        // if index is root use  pop
         if (index == 0) {
             pop();
 
+        // else change the nodes value to -INF sort the heap and use pop to remove it    
         } else {
 
             int value = Integer.MIN_VALUE;
@@ -45,19 +58,26 @@ public class MinBinaryHeap extends Heaps {
 
     }
 
+    /**
+     * Removes the node with the smallest value and returns it.
+     * @return the smallest node of the heap
+     */
     @Override
     public int pop() {
 
+        // save the root value
         int rootValue = this.heap.get(0);
 
         int last = this.heap.size() - 1;
-
+        //check if the root is the only node
+        // else swap the root to the end of the heap, remove it
         if (last != 0) {
 
             super.swap(0, last);
 
             this.heap.remove(last);
 
+            //sift the new root down to keep the heap sorted 
             siftDown(0);
 
         }
@@ -66,12 +86,18 @@ public class MinBinaryHeap extends Heaps {
 
     }
 
+    /**Sift down a node from the given index to it's proper level to sustain heap property
+     * 
+     * @param index where to start the sifting
+     */
     @Override
     protected void siftDown(int index) {
 
         int last = this.heap.size() - 1;
         int value = this.heap.get(index);
 
+        //swap the given node with one of its children downwards 
+        //till it's childrens have a greater value or the node is a leaf
         while (leftChildIndex(index) <= last) {
 
             int left = leftChild(index);
@@ -89,13 +115,19 @@ public class MinBinaryHeap extends Heaps {
             }
         }
     }
-
+    
+ /**Sift up a node from the given index to it's proper level to sustain heap property
+     * 
+     * @param index where to start the sifting
+     */
     @Override
     protected void siftUp(int index) {
 
         int value = this.heap.get(index);
         int parent = this.parent(index);
-
+        
+        //swap node with its parent till its parent is smaller 
+        //or the node is root
         while (index >= 0 && parent > value) {
 
             swap(parentIndex(index), index);
